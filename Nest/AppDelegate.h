88ba70187b4fce00d6e821b9f133b3bb74020a03
ivanSamalazau <isamalazau@scnsoft.com>
2016-01-14 11:05:50 +0300

    Initial Commit

---
+++ Nest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nest
+//
+//  Created by ivanSamalazau on 14.1.16.
+//  Copyright © 2016 ScienceSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nest
+//
+//  Created by ivanSamalazau on 14.1.16.
+//  Copyright © 2016 ScienceSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
