62384fd1c3ed1c41b4f80e919ff957651846eccf
ivanSamalazau <isamalazau@scnsoft.com>
2016-01-14 14:52:02 +0300

    Refactored file structure

--- Nest/ViewControllers/ViewController.m
+++ Nest/ViewControllers/ViewController.m
@@ -14,14 +14,24 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Actions
+
+- (IBAction)onLogin:(UIButton *)sender
+{
+    
+}
+
 
 @end
